f2adbb5b9c927c7cb1da6e5aa8bb347a4fb37ad2
Ana Enríquez <ann_eav@hotmail.com>
2016-05-31 22:07:38 +0200

    Finalizado el taller, ha faltado dar bottlepy

--- taiga.py
+++ taiga.py
@@ -1,6 +1,33 @@
 # importamos requests
+import csv
 import requests
-# cogemos datos desde una url en un requests
-response = requests.get("https://api.taiga.io/api/v1/projects")
+def get_activity(x):
+    return x['total_activity']
+# cogemos datos desde una url en un requests (?page_size=20 nos ayuda a que sea más rápido cogiendo 20)
+response = requests.get("https://api.taiga.io/api/v1/projects?page_size=20")
 # imprimimos response para ver que ha hecho (si devuelve 200 significa ok)
-print (response)
+# print (response)
+# le decimos que projects es el json de response
+projects = response.json()
+# print(projects)
+# print(len(projects))
+# para crear una lista vacia
+result = []
+# creamos proyectos
+for project in projects:
+    result.append({
+        'id': project['id'],
+        'name': project['name'],
+        'description': project['description'],
+        'owner': project['owner']['full_name_display'],
+        'total_activity': project['total_activity']
+    })
+
+result.sort(key=get_activity, reverse=True)
+# abrimos el archivo csv y lo escribimos con el array de la api
+with open('projects.csv', 'w') as csvfile:
+    fieldnames = ['total_activity', 'id', 'name', 'description', 'owner']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
+    writer.writeheader()
+    for r in result:
+        writer.writerow(r)
